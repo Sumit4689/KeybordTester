924bb140c01e3f971d75333391c92fa29b193baf
Sumit Subhash Patil <sumitgatpatil41@gmail.com>
2025-07-11 00:41:19 +0530

    done with keyboard totally

--- src/components/Keyboard2.jsx
+++ src/components/Keyboard2.jsx
@@ -1,7 +1,26 @@
 import NavBar from "./NavBar";
 import "./keyboard2.css";
+import { use, useEffect } from "react";
 
 function Keyboard2() {
+    useEffect(() => {
+        const keyboard = document.querySelector('.keyboard-container');
+        const keys = keyboard.querySelectorAll('.key');
+        const keycaps = keyboard.querySelectorAll('.keycap');
+        const keycapsArray = Array.from(keycaps);
+        const keyArray = Array.from(keys);
+        const keyMap = keycapsArray.reduce((acc, keycap, index) => {
+            acc[keycap.textContent.trim()] = keyArray[index];
+            return acc;
+        }, {});
+        const handleKeyDown = (event) => {
+            const key = event.key;
+            if (keyMap[key]) {
+                keyMap[key].classList.add('pressed');
+            }
+        };
+    })
+
     return (
         <main className="min-h-screen flex flex-col bg-[var(--background)]">
             <NavBar />
@@ -24,25 +43,25 @@ function Keyboard2() {
                     <div className="keyboard-container bg-[var(--card-background)] rounded-lg p-6">
                         <div className="function-key">
                             <div className="singlekey">
-                                <div className="key"><div className="keycap"><span className="bottom">Esc</span></div></div>
+                                <div className="key escape-key"><div className="keycap escape-key"><span className="bottom">Esc</span></div></div>
                             </div>
                             <div className="key-cluster">
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F1</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F2</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F3</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F4</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F1</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F2</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F3</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F4</span></div></div>
                             </div>
                             <div className="key-cluster">
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F5</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F6</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F7</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F8</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F5</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F6</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F7</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F8</span></div></div>
                             </div>
                             <div className="key-cluster">
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F9</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F10</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F11</span></div></div>
-                                <div className="key function-row"><div className="keycap"><span className="bottom">F12</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F9</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F10</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F11</span></div></div>
+                                <div className="key function-row functions-key"><div className="keycap functions-key"><span className="bottom">F12</span></div></div>
                             </div>
                             <div className="singlekey">
                                 <div className="key"><div className="keycap"><span className="bottom">Del</span></div></div>
@@ -71,12 +90,12 @@ function Keyboard2() {
                                 <div className="key"><div className="keycap"><span className="top">)</span><span className="bottom">0</span></div></div>
                                 <div className="key"><div className="keycap"><span className="top">_</span><span className="bottom">-</span></div></div>
                                 <div className="key"><div className="keycap"><span className="top">+</span><span className="bottom">=</span></div></div>
-                                <div className="key key-backspace"><div className="keycap"><span className="bottom">Backspace</span></div></div>
+                                <div className="key key-backspace modifier-key"><div className="keycap modifier-key"><span className="bottom">Backspace</span></div></div>
                             </div>
                             <div className="qwerty-first">
                                 <div className="key key-wide"><div className="keycap"><span className="bottom">Tab</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">Q</span></div></div>
-                                <div className="key"><div className="keycap"><span className="bottom">W</span></div></div>
+                                <div className="key wasd-key"><div className="keycap wasd-key"><span className="bottom">W</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">E</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">R</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">T</span></div></div>
@@ -91,9 +110,9 @@ function Keyboard2() {
                             </div>
                             <div className="qwerty-second">
                                 <div className="key key-caps"><div className="keycap"><span className="bottom">Caps Lock</span></div></div>
-                                <div className="key"><div className="keycap"><span className="bottom">A</span></div></div>
-                                <div className="key"><div className="keycap"><span className="bottom">S</span></div></div>
-                                <div className="key"><div className="keycap"><span className="bottom">D</span></div></div>
+                                <div className="key wasd-key"><div className="keycap wasd-key"><span className="bottom">A</span></div></div>
+                                <div className="key wasd-key"><div className="keycap wasd-key"><span className="bottom">S</span></div></div>
+                                <div className="key wasd-key"><div className="keycap wasd-key"><span className="bottom">D</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">F</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">G</span></div></div>
                                 <div className="key"><div className="keycap"><span className="bottom">H</span></div></div>
@@ -102,11 +121,11 @@ function Keyboard2() {
                                 <div className="key"><div className="keycap"><span className="bottom">L</span></div></div>
                                 <div className="key"><div className="keycap"><span className="top">:</span><span className="bottom">;</span></div></div>
                                 <div className="key"><div className="keycap"><span className="top">"</span><span className="bottom">'</span></div></div>
-                                <div className="key key-enter"><div className="keycap"><span className="bottom">Enter</span></div></div>
+                                <div className="key key-enter wasd-key"><div className="keycap wasd-key"><span className="bottom">Enter</span></div></div>
                             </div>
                             <div className="qwerty-third">
                                 <div className="qwerty-third-left">
-                                    <div className="key key-shift-Left"><div className="keycap"><span className="bottom">Shift</span></div></div>
+                                    <div className="key key-shift-Left modifier-key"><div className="keycap modifier-key"><span className="bottom">Shift</span></div></div>
                                     <div className="key"><div className="keycap"><span className="bottom">Z</span></div></div>
                                     <div className="key"><div className="keycap"><span className="bottom">X</span></div></div>
                                     <div className="key"><div className="keycap"><span className="bottom">C</span></div></div>
@@ -117,25 +136,25 @@ function Keyboard2() {
                                     <div className="key"><div className="keycap"><span className="top">&lt;</span><span className="bottom">,</span></div></div>
                                     <div className="key"><div className="keycap"><span className="top">&gt;</span><span className="bottom">.</span></div></div>
                                     <div className="key"><div className="keycap"><span className="top">?</span><span className="bottom">/</span></div></div>
-                                    <div className="key key-shift-Right"><div className="keycap"><span className="bottom">Shift</span></div></div>
+                                    <div className="key key-shift-Right modifier-key"><div className="keycap modifier-key"><span className="bottom">Shift</span></div></div>
                                 </div>
                                 <div className="qwerty-third-right">
-                                    <div className="key"><div className="keycap"><span className="bottom">↑</span></div></div>
+                                    <div className="key arrow-key"><div className="keycap arrow-key"><span className="bottom">↑</span></div></div>
                                 </div>
                             </div>
                             <div className="qwerty-bottom">
                                 <div className="qwerty-bottom-left">
-                                    <div className="key key-ctrl"><div className="keycap"><span className="bottom">Ctrl</span></div></div>
+                                    <div className="key key-ctrl modifier-key"><div className="keycap modifier-key"><span className="bottom">Ctrl</span></div></div>
                                     <div className="key key-ctrl"><div className="keycap"><span className="bottom">⊞</span></div></div>
-                                    <div className="key key-alt"><div className="keycap"><span className="bottom">Alt</span></div></div>
+                                    <div className="key key-alt modifier-key"><div className="keycap modifier-key"><span className="bottom">Alt</span></div></div>
                                     <div className="key key-space"><div className="keycap"></div></div>
-                                    <div className="key"><div className="keycap"><span className="bottom">Alt</span></div></div>
+                                    <div className="key modifier-key"><div className="keycap modifier-key"><span className="bottom">Alt</span></div></div>
                                     <div className="key"><div className="keycap"><span className="bottom">Fn</span></div></div>
-                                    <div className="key"><div className="keycap"><span className="bottom">Ctrl</span></div></div>
+                                    <div className="key modifier-key"><div className="keycap modifier-key"><span className="bottom">Ctrl</span></div></div>
                                 </div> 
                                 <div className="qwerty-bottom-right">
-                                    <div className="key key-downArrow"><div className="keycap"><span className="bottom">←</span></div></div>
-                                    <div className="key key-downArrow"><div className="keycap"><span className="bottom">↓</span></div></div>
+                                    <div className="key key-downArrow arrow-key"><div className="keycap arrow-key"><span className="bottom">←</span></div></div>
+                                    <div className="key key-downArrow arrow-key"><div className="keycap arrow-key"><span className="bottom">↓</span></div></div>
                                 </div>
                             </div>
                         </div>
@@ -169,7 +188,7 @@ function Keyboard2() {
                                     <div className="key numpad-plus"><div className="keycap"><span className="bottom">↵</span></div></div>
                                 </div>
                                 <div className="singlekey2 key-rightArrow">
-                                    <div className="key key-downArrow"><div className="keycap"><span className="bottom">→</span></div></div>
+                                    <div className="key key-downArrow arrow-key"><div className="keycap arrow-key"><span className="bottom">→</span></div></div>
                                 </div>
                                 <div className="singlekey2 ">
                                     <div className="key"><div className="keycap"><span className="bottom">0</span></div></div>
